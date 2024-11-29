30513ebf81f0892f46109bd77321ccecc9380501
meryam <meryemsalhi590@gmail.com>
2024-11-29 10:40:49 +0100

    first commit

--- main.js
+++ main.js
@@ -21,9 +21,9 @@ position.addEventListener("change", function (event) {
     }
 });
 
-var select = document.getElementById('position');
 function addJoueur() {
-
+var select = document.getElementById('position');
+    
     let post = document.getElementById(`${select.value}`);
     let imgJoueur = post.querySelector('img')
     let inputs = document.getElementById('url')
@@ -33,6 +33,23 @@ function addJoueur() {
     statique[0].textContent = rt.value;
     let nom = document.getElementById('name')
     statique[2].textContent = nom.value;
+
+    if (select.value === "GK"){
+        
+    let dv = document.getElementById('diving');
+    let hd = document.getElementById('handling');
+    let kc = document.getElementById('kicking');
+    let rf = document.getElementById('reflexes');
+    let sp = document.getElementById('speed');
+    let ps = document.getElementById('positioning');
+    statique[4].textContent = dv.value;
+    statique[6].textContent = hd.value;
+    statique[8].textContent = kc.value;
+    statique[10].textContent = rf.value;
+    statique[12].textContent = sp.value;
+    statique[14].textContent = ps.value;
+    
+    }else{
         let pc = document.getElementById('pace');
         let st = document.getElementById('shooting');
         let psg = document.getElementById('passing');
@@ -45,5 +62,5 @@ function addJoueur() {
         statique[10].textContent = dr.value;
         statique[12].textContent = df.value;
         statique[14].textContent = ph.value;
-
+      }
 }
